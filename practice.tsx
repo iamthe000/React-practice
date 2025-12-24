6361e3a6ed56c9c9947a663f415f5799fdd2b3a7
minimium <umekichi3hoshi@gmail.com>
2025-12-24 18:54:32 +0900

    Update practice.tsx

--- practice.tsx
+++ practice.tsx
@@ -8,21 +8,29 @@ function App() {
   //[現在の値, 更新するための関数] = useState('') によって更新関数とすることができる
   const [msg, setMsg] = useState('');
 
+  const [bigMsg, setBM] = useState("test")
+
   const handleClick = () => {
     //直接書き換えずに、更新関数を呼ぶ
     setMsg('Clicked');
+    setBM("BIG MESSAGE")
   };
 
   return (
-    <div className="App">
+    <div className="App p-4">
       <h1>{text}</h1>
       <button onClick={handleClick} className="bg-black text-white p-2 rounded">
         {/*bg:背景 text:文字色 p:padding(1あたり4px) rounded:角丸*/}
         Click Me
       </button>
-      
       {/* 変数 msg の中身が表示される（自動で更新される) */}
-      <div id="msg">{msg}</div>
+      <div id="msg" className="mb-10">{msg}</div>
+      <h1 className="bg-gradient-to-r from-green-400 via-cyan-900 to-blue-500 bg-clip-text text-transparent text-5xl font-bold text-9xl">{/* 普通はこのサイズが限界 */}
+        {bigMsg}
+      </h1>
+      <h1 className="bg-gradient-to-r from-red-500 via-blue-900 to-blue-500 bg-clip-text text-transparent text-5xl font-bold text-[200px]">{/* 普通はこのサイズが限界 */}
+        BIG!
+      </h1>
     </div>
   );
 }
